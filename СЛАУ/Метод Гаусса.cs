9bf46ceaf4be818f2a6ed673c4d17e512b38e169
Myashka <57448108+Myashka@users.noreply.github.com>
2021-03-12 18:22:18 +0300

    Add files via upload

---
+++ СЛАУ/Метод Гаусса.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Метод_Гаусса
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Здравствуйте, введите размерность матрицы: ");
+            double s, d;
+            int n = Int32.Parse(Console.ReadLine());
+            double[,] a = new double[n, n];
+            double[] x = new double[n];
+            double[] b = new double[n];
+
+            Console.WriteLine("Введите построчно коэффициенты системы: x1, x2, x3, x(n)");
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = double.Parse(Console.ReadLine());
+                }
+            //вывод матрица A
+            Console.WriteLine("Получившаяся матрица A:");
+            Show_Boxy_Matrix(a, n);
+            Console.WriteLine("\nВведите коэффициенты y1, y2, y3, y(n)");
+            for (int i = 0; i < n; i++)
+            {
+                b[i] = double.Parse(Console.ReadLine());
+            }
+            //вывод матрица b
+            Console.WriteLine("\nПолучившаяся матрица B:");
+            Show_One_Matrix_B(b, n);
+
+            // прямой ход метода Гаусса 
+            for (int k = 0; k < n - 1; k++)
+            {
+                for (int i = k + 1; i < n; i++)
+                {
+                    d = a[i, k] / a[k, k];
+                    for (int j = k; j < n; j++)
+                    {
+                        a[i, j] = a[i, j] - a[k, j] * d;
+                    }
+                    b[i] = b[i] - b[k] * d;
+                }
+            }
+
+            //вывод матрица A после преобразований
+            Console.WriteLine("\nМатрица A после приведения к ступенчатому виду:");
+            Show_Boxy_Matrix(a, n);
+            Console.WriteLine("\nМатрица B после прямого хода:");
+            Show_One_Matrix_B(b, n);
+
+            //обратный ход
+            for (int k = n - 1; k >= 0; k--)
+            {
+                s = 0;
+                for (int i = k; i <= n-1; i++)
+                {
+                    s += a[k, i] * x[i];
+                }
+                if (a[k,k] == 0)
+                {
+                    Console.WriteLine("Элемент A", k, ',', k, "= 0");
+                    Environment.Exit(0);
+                }
+                x[k] = (b[k] - s)/a[k,k];
+            }
+            Console.WriteLine("\nКорни системы:");
+            Show_One_Matrix_X(x, n);
+        }
+
+
+        static void Show_One_Matrix_X(double [] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"x[{i}] = {Math.Round(a[i], 4)}");
+
+            }
+        }
+        static void Show_One_Matrix_B(double[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"B[{i}] = {Math.Round(a[i], 4)}");
+
+            }
+        }
+        static void Show_Boxy_Matrix(double [,] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(Math.Round(a[i, j], 4) + "\t");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ СЛАУ/Метод прогонки.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Метод_прогонки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Здравствуйте, введите размерность матрицы: ");
+            int n = Int32.Parse(Console.ReadLine());
+            double[,] A = new double[n, n];
+            double[] X = new double[n];
+            double[] B = new double[n];
+            double[] a = new double[n];
+            double[] b = new double[n];
+            double[] e = new double[n];
+            Console.WriteLine("Введите построчно коэффициенты системы: x1, x2, x3, x(n)");
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = double.Parse(Console.ReadLine());
+                }
+
+            //вывод матрица A
+            Console.WriteLine("\nПолучившаяся матрица A:");
+            Show_Boxy_Matrix(A, n);
+
+            Console.WriteLine("\nВведите коэффициенты y1, y2, y3, y(n)");
+            for (int i = 0; i < n; i++)
+            {
+                B[i] = double.Parse(Console.ReadLine());
+            }
+
+            //вывод матрица b
+            Console.WriteLine("\nПолучившаяся матрица B:");
+            Show_One_Matrix_B(B, n);
+
+            // прямой ход прогонки
+            e[0] = A[0, 0];
+            a[0] = -A[0, 1] / e[0];
+            b[0] = B[0] / e[0];
+            for (int i = 1; i < n-1; i++)
+            {
+                if (A[i, i] == 0)
+                {
+                    Console.WriteLine("Элемент A", i, ',', i, "= 0");
+                    Environment.Exit(0);
+                }
+                e[i] = A[i, i] + A[i, i - 1] * a[i - 1];
+                a[i] = -A[i, i + 1] / e[i];
+                b[i] = (B[i] - A[i, i - 1] * b[i - 1]) / e[i];
+            }
+            e[n - 1] = A[n - 1, n - 1] + A[n - 1, n - 2] * a[n - 2];
+            b[n - 1] = (B[n - 1] - A[n - 1, n - 2] * b[n - 2]) / e[n - 1];
+
+            //обратный ход прогонки
+            X[n - 1] = b[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+            {
+                X[i] = a[i] * X[i + 1] + b[i];
+            }
+
+            Console.WriteLine("\nКорни системы:");
+            Show_One_Matrix_X(X, n);
+        }
+
+
+        static void Show_One_Matrix_X(double[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"x[{i}] = {Math.Round(a[i],4)}");
+
+            }
+        }
+        static void Show_One_Matrix_B(double[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"B[{i}] = {a[i]}");
+
+            }
+        }
+        static void Show_Boxy_Matrix(double[,] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(a[i, j] + "\t");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
